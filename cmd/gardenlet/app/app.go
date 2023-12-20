d44bb2e9574207bd66827c11050688f3baf896c2
Shafeeque E S <shafeeque.e.s@sap.com>
2023-12-20 11:00:06 +0530

    Cleanup addons MR

--- cmd/gardenlet/app/app.go
+++ cmd/gardenlet/app/app.go
@@ -79,6 +79,7 @@ import (
 	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
 	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
 	utilclient "github.com/gardener/gardener/pkg/utils/kubernetes/client"
+	"github.com/gardener/gardener/pkg/utils/managedresources"
 	thirdpartyapiutil "github.com/gardener/gardener/third_party/controller-runtime/pkg/apiutil"
 )
 
@@ -393,6 +394,12 @@ func (g *garden) Start(ctx context.Context) error {
 		return err
 	}
 
+	// TODO(shafeeqes): Remove this in gardener v1.90
+	log.Info("Cleaning up legacy 'shoot-core' ManagedResource")
+	if err := cleanupShootCoreManagedResource(ctx, g.mgr.GetClient()); err != nil {
+		return err
+	}
+
 	log.Info("Setting up shoot client map")
 	shootClientMap, err := clientmapbuilder.
 		NewShootClientMapBuilder().
@@ -689,6 +696,30 @@ func getSecretsToRecreate(ctx context.Context, c client.Client, namespacesInDele
 		return namespacesInDeletion.Has(s.Namespace) || !slices.Contains(s.Finalizers, grmFinalizer) || s.DeletionTimestamp == nil
 	})
 	return secretsToRecreate, nil
+}
+
+func cleanupShootCoreManagedResource(ctx context.Context, seedClient client.Client) error {
+	managedResourceList := &metav1.PartialObjectMetadataList{}
+	managedResourceList.SetGroupVersionKind(resourcesv1alpha1.SchemeGroupVersion.WithKind("ManagedResourceList"))
+	if err := seedClient.List(ctx, managedResourceList); err != nil {
+		if meta.IsNoMatchError(err) {
+			return nil
+		}
+		return err
+	}
+
+	var taskFns []flow.TaskFn
+	for _, managedResource := range managedResourceList.Items {
+		if managedResource.GetName() == "shoot-core" {
+			mr := managedResource
+
+			taskFns = append(taskFns, func(ctx context.Context) error {
+				return managedresources.DeleteForShoot(ctx, seedClient, mr.Namespace, "shoot-core")
+			})
+		}
+	}
+
+	return flow.Parallel(taskFns...)(ctx)
 }
 
 func (g *garden) registerSeed(ctx context.Context, gardenClient client.Client) error {
